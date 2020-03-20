6ad8e60322c65efe4cf973b56d6c39499ed0f717
baravak <itb.baravak@gmail.com>
2020-03-20 18:43:04 +0330

    add helpers

---
+++ src/helpers.php
@@ -0,0 +1,23 @@
+<?php
+function text2summary($str, $n = 500, $end_char = '&#8230;')
+{
+    if (mb_strlen($str) < $n) {
+        return $str;
+    }
+
+    $str = preg_replace("/\s+/", ' ', str_replace(array("\r\n", "\r", "\n"), ' ', $str));
+
+    if (strlen($str) <= $n) {
+        return $str;
+    }
+
+    $out = "";
+    foreach (explode(' ', trim($str)) as $val) {
+        $out .= $val . ' ';
+
+        if (mb_strlen($out) >= $n) {
+            $out = trim($out);
+            return (mb_strlen($out) == mb_strlen($str)) ? $out : $out . $end_char;
+        }
+    }
+}
