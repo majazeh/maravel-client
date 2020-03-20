86f8cb5d300e768dfb3ce2949c98a66ba0d4eb1d
baravak <itb.baravak@gmail.com>
2020-03-20 11:55:23 +0330

    bugfix

--- src/views/dashboard/users/index.blade.php
+++ src/views/dashboard/users/index.blade.php
@@ -77,6 +77,9 @@
                     </tbody>
                 </table>
             </div>
+            <div class="d-flex justify-content-center">
+                {{$posts->render()}}
+            </div>
         </div>
     </div>
 @endsection
