58aaf0265ca1ba94c3bbae3399fd9e10a6dcd240
baravak <itb.baravak@gmail.com>
2020-08-22 20:06:36 +0430

    update sarkoot and theory

--- src/views/auth/theory.blade.php
+++ src/views/auth/theory.blade.php
@@ -1,12 +1,18 @@
 @section('auth-nav')
 <div class="d-flex justify-content-center">
-    <a href="{{route('auth')}}" class="text-light text-decoration-none font-weight-bold fs-14">{{__('Login')}}</a>
-    @if (config('auth.registration', true))
+    @if (!auth()->check())
+        <a href="{{route('auth')}}" class="text-light text-decoration-none font-weight-bold fs-14">{{__('Login')}}</a>
+        @if (config('auth.registration', true))
+            <span class="px-2 text-white">|</span>
+            <a href="{{route('register')}}" class="text-light text-decoration-none font-weight-bold fs-14">{{__('Register')}}</a>
+        @endif
         <span class="px-2 text-white">|</span>
-        <a href="{{route('register')}}" class="text-light text-decoration-none font-weight-bold fs-14">{{__('Register')}}</a>
+        <a href="{{route('auth.recovery')}}" class="text-light text-decoration-none fs-14">{{__('Forgot Password')}}</a>
+    @else
+        <a href="{{route('dashboard.home')}}" class="text-light text-decoration-none fs-14 font-weight-bold direct">{{__('Dashboard')}}</a>
+            <span class="px-2 text-white">|</span>
+        <a href="{{route('logout')}}" data-lijax="click" data-method="POST" class="text-light text-decoration-none fs-14">{{__('Logout')}}</a>
     @endif
-    <span class="px-2 text-white">|</span>
-    <a href="{{route('auth.recovery')}}" class="text-light text-decoration-none fs-14">{{__('Forgot Password')}}</a>
 </div>
 @endsection
 @extends($ajax ? 'auth.xhr' : 'auth.app')
