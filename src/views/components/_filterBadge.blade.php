a12b4ce01a6aeb467d5b1a21787d0b170aeab9e5
abouhatef <itb.baravak@gmail.com>
2020-12-16 15:42:22 +0330

    filter badge bugfix

--- src/views/components/_filterBadge.blade.php
+++ src/views/components/_filterBadge.blade.php
@@ -12,7 +12,14 @@
                 {{__(ucfirst($key))}}: {{__(ucfirst($item))}}
             </span>
         @endforeach
-    @else
+    @elseif($model->getFilter($key) instanceof \Illuminate\Database\Eloquent\Model)
+        <span class="badge badge-secondary font-weight-normal">
+            <a href="{{app('request')->create($model->url($model->currentPage()), 'GET', [$key => null])->getUri()}}" class="text-decoration-none">
+                <i class="fal fa-times align-middle"></i>
+            </a>
+            {{__(ucfirst($key))}}: {{__($model->getFilter($key)->getFilterValue())}}
+        </span>
+    @elseif(!is_object($value))
         <span class="badge badge-secondary font-weight-normal">
             <a href="{{app('request')->create($model->url($model->currentPage()), 'GET', [$key => null])->getUri()}}" class="text-decoration-none">
                 <i class="fal fa-times align-middle"></i>
