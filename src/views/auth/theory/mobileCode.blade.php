1b5ab80e99aa6ed514d9f6e2681b277693bcefc8
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-02-13 16:26:35 +0330

    Minor fix

--- src/views/auth/theory/mobileCode.blade.php
+++ src/views/auth/theory/mobileCode.blade.php
@@ -1,8 +1,9 @@
 @section('auth-form')
-<div class="form-group">
-    <input type="password" class="form-control" id="code" name="code" placeholder="{{__('Code')}}">
-</div>
+    <div class="mb-2">
+        <input type="password" class="w-full text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded-sm" id="code" name="code" placeholder="{{ __('Code')}} ">
+    </div>
 
-<button class="btn btn-dark btn-block btn-login mb-3">{{__('Check')}}</button>
+    <button class="w-full h-10 text-sm rounded-full bg-blue-600 text-white hover:bg-blue-700 transition mb-4">{{ __('Check') }}</button>
 @endsection
+
 @extends('auth.theory')
--- src/views/auth/theory/password.blade.php
+++ src/views/auth/theory/password.blade.php
@@ -1,8 +1,9 @@
 @section('auth-form')
-<div class="form-group">
-    <input type="password" class="form-control" id="password" name="password" placeholder="{{__('Password')}}">
-</div>
+    <div class="mb-4">
+        <input type="password" class="w-full text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded-sm" id="password" name="password" placeholder="{{__('Password')}}">
+    </div>
 
-<button class="btn btn-dark btn-block btn-login mb-3">{{__('Password')}}</button>
+    <button class="w-full h-10 text-sm rounded-full bg-blue-600 text-white hover:bg-blue-700 transition mb-4">{{ __('Password') }}</button>
 @endsection
+
 @extends('auth.theory')
