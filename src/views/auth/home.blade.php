a1c26fe5a32cecae5a9cd702a5bf3a8bdb94ada4
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-02-13 16:10:34 +0330

    Minor fix

--- src/views/auth/home.blade.php
+++ src/views/auth/home.blade.php
@@ -1,29 +1,29 @@
 @section('auth-form')
-@if (request()->callback)
-    <div class="mb-1">
-        {{ __("To continue the process you need to login or register") }}
+    @if (request()->callback)
+        <div class="mb-1">
+            {{ __("To continue the process you need to login or register") }}
+        </div>
+    @endif
+    <div class="mb-4">
+        <input type="text" class="w-full text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded-sm" id="authorized_key" name="authorized_key" value="{{ app('request')->authorized_key }}" placeholder="{{ auth()->check() ? __('Entry Command') : __('Phone, Email or Username') }}">
     </div>
-@endif
-<div class="mb-4">
-    <input type="text" class="w-full text-sm text-left dir-ltr border border-gray-200 rounded-sm" id="authorized_key" name="authorized_key" value="{{ app('request')->authorized_key }}" placeholder="{{ auth()->check() ? __('Entry Command') : __('Phone, Email or Username') }}">
-</div>
-
-<button class="w-full h-10 text-sm rounded-full bg-blue-600 text-white hover:bg-blue-700 transition mb-4">{{ auth()->check() ? __('Check') : __('Enter') }}</button>
+    <button class="w-full h-10 text-sm rounded-full bg-blue-600 text-white hover:bg-blue-700 transition mb-4">{{ auth()->check() ? __('Check') : __('Enter') }}</button>
 @endsection
 
 @section('auth-nav')
-<div class="flex justify-center">
-    @if (auth()->check())
-        <a href="{{ route('dashboard.home') }}" class="font-bold direct">{{ __('Dashboard') }}</a>
+    <div class="flex justify-center">
+        @if (auth()->check())
+            <a href="{{ route('dashboard.home') }}" class="font-bold direct">{{ __('Dashboard') }}</a>
             <span class="px-4 text-gray-500">|</span>
-        <a href="{{ route('logout') }}" data-lijax="click" data-method="POST">{{__('Logout')}}</a>
-    @else
-        @if (config('auth.registration', true))
-            <a href="{{ route('register', ['callback' => request()->callback]) }}" class="text-sm font-bold text-gray-700 hover:text-gray-900 transition">{{ __('Register') }}</a>
+            <a href="{{ route('logout') }}" data-lijax="click" data-method="POST">{{__('Logout')}}</a>
+        @else
+            @if (config('auth.registration', true))
+                <a href="{{ route('register', ['callback' => request()->callback]) }}" class="text-sm font-bold text-gray-700 hover:text-gray-900 transition">{{ __('Register') }}</a>
+            @endif
+            <span class="px-4 text-gray-500">|</span>
+            <a href="{{ route('auth.recovery') }}" class="text-sm text-gray-700 hover:text-gray-900 transition">{{ __('Forgot Password') }}</a>
         @endif
-        <span class="px-4 text-gray-500">|</span>
-        <a href="{{ route('auth.recovery') }}" class="text-sm text-gray-700 hover:text-gray-900 transition">{{ __('Forgot Password') }}</a>
-    @endif
-</div>
+    </div>
 @endsection
+
 @extends($ajax ? 'auth.xhr' : 'auth.app')
--- src/views/auth/recovery.blade.php
+++ src/views/auth/recovery.blade.php
@@ -1,16 +1,16 @@
 @section('auth-form')
-<div class="mb-4">
-    <input type="text" class="text-sm text-left dir-ltr border border-gray-200 rounded-sm" id="username" name="username" placeholder="{{ __('Mobile') }}">
-</div>
-
-<button class="text-sm w-full rounded-full h-10 bg-blue-600 text-white hover:bg-blue-700 transition mb-4">{{ __('Receive code') }}</button>
+    <div class="mb-4">
+        <input type="text" class="w-full text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded-sm" id="username" name="username" placeholder="{{ __('Mobile') }}">
+    </div>
+    <button class="text-sm w-full rounded-full h-10 bg-blue-600 text-white hover:bg-blue-700 transition mb-4">{{ __('Receive code') }}</button>
 @endsection
 
 @section('auth-nav')
-<div class="flex justify-center">
-    <a href="{{ route('auth') }}" class="text-sm text-gray-700 hover:text-gray-900 transition">{{ __('Login') }}</a>
-    <span class="px-4 text-gray-500">|</span>
-    <a href="{{ route('register') }}" class="text-sm text-gray-700 hover:text-gray-900 transition">{{ __('Register') }}</a>
-</div>
+    <div class="flex justify-center">
+        <a href="{{ route('auth') }}" class="text-sm text-gray-700 hover:text-gray-900 transition">{{ __('Login') }}</a>
+        <span class="px-4 text-gray-500">|</span>
+        <a href="{{ route('register') }}" class="text-sm text-gray-700 hover:text-gray-900 transition">{{ __('Register') }}</a>
+    </div>
 @endsection
+
 @extends('auth.theory')
