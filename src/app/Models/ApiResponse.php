f36a66aaa58f9975d64773b03915bbed83f36ce2
abouhatef <itb.baravak@gmail.com>
2021-03-26 19:52:20 +0430

    redirect if lost session

--- src/app/Models/ApiResponse.php
+++ src/app/Models/ApiResponse.php
@@ -46,7 +46,8 @@ class ApiResponse implements Arrayable
         if (isset($this->response->url)) {
             $this->response->url = str_replace(API::path(), app('request')->getSchemeAndHttpHost() . '/', $this->response->url);
         }
-        if($this->response->message == 'UNAUTHENTICATED')
+
+        if($this->response->message == 'UNAUTHENTICATED.')
         {
             session()->put('APIToken', null);
             return redirect()->route('auth')->send();
